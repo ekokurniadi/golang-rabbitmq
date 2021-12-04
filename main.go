78c85e00cca7f6bff133c40c636388386151c800
Ekokurniadi <ekokurniadi.02@gmail.com>
2021-12-04 15:31:34 +0700

    initials commit with setting connection to RabbitMQ Instance

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	fmt.Println("Go RabbitMQ Tutorial")
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Successfully connected to our RabbitMQ instance")
+
+}
